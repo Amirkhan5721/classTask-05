70b2c39a7810cfc09497b8ffbe8cf545bb973b52
Amirkhan5721 <amirkhanafridi959@gmail.com>
2024-09-29 11:29:05 -0700

    select all elements id through DOM in JS file

---
+++ script.js
@@ -0,0 +1,5 @@
+var dobInputs = document.getElementById("dob");
+var calculateBtn = document.getElementById("calc-btn");
+var yearsElement = document.getElementById("years");
+var monthsElement = document.getElementById("months");
+var daysElement = document.getElementById("days");
